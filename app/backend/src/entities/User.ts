4ed29eaf29128f07077360af94768f76f9297ae5
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-12-03 15:14:21 -0300

    feat: user entity

---
+++ app/backend/src/entities/User.ts
@@ -0,0 +1,25 @@
+import { CreateDateColumn, Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column({ length: 11 })
+  cpf: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
