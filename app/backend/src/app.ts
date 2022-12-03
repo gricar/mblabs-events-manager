3818605b02622bbe8a435a00498d4711a80f1739
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-12-03 16:31:08 -0300

    feat: error handler middleware

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import errorHandler from './middlewares/errorHandler';
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+
+app.use(errorHandler);
 
 export { app };
