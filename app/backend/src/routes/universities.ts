26745f65ae752691f9da06e28b84dad8b3e17e8b
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-12-05 12:00:41 -0300

    feat: authenticated routes

--- app/backend/src/routes/universities.ts
+++ app/backend/src/routes/universities.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import UniversitiesController from '../controllers/UniversitiesController';
+import auth from '../middlewares/auth';
 
 const universities = Router();
 
-universities.get('/', UniversitiesController.getAll);
 universities.post('/', UniversitiesController.create);
+universities.use(auth);
+universities.get('/', UniversitiesController.getAll);
 universities.get('/:id', UniversitiesController.getById);
 universities.put('/:id', UniversitiesController.update);
 universities.delete('/:id', UniversitiesController.remove);
--- app/backend/src/routes/users.ts
+++ app/backend/src/routes/users.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import usersController from '../controllers/UsersController';
+import auth from '../middlewares/auth';
 
 const users = Router();
 
-users.get('/', usersController.getAll);
 users.post('/', usersController.create);
+users.use(auth);
+users.get('/', usersController.getAll);
 users.get('/:id', usersController.getById);
 users.put('/:id', usersController.update);
 users.delete('/:id', usersController.remove);
