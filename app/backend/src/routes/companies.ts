8eae3790a845f79af25661fb86d82ddea40056f9
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-12-06 16:40:27 -0300

    feat: included midd at edit route

--- app/backend/src/routes/companies.ts
+++ app/backend/src/routes/companies.ts
@@ -9,7 +9,7 @@ companies.post('/', validateCompany, CompaniesController.create);
 companies.use(auth);
 companies.get('/', CompaniesController.getAll);
 companies.get('/:id', CompaniesController.getById);
-companies.put('/:id', CompaniesController.update);
+companies.put('/:id', validateCompany, CompaniesController.update);
 companies.delete('/:id', CompaniesController.remove);
 
 export default companies;
--- app/backend/src/routes/universities.ts
+++ app/backend/src/routes/universities.ts
@@ -9,7 +9,7 @@ universities.post('/', validateUniversity, UniversitiesController.create);
 universities.use(auth);
 universities.get('/', UniversitiesController.getAll);
 universities.get('/:id', UniversitiesController.getById);
-universities.put('/:id', UniversitiesController.update);
+universities.put('/:id', validateUniversity, UniversitiesController.update);
 universities.delete('/:id', UniversitiesController.remove);
 
 export default universities;
