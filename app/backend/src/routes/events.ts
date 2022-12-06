e4387ebdc2c0f729ee4450bc90c8014776b461f9
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-12-06 17:52:23 -0300

    feat: validate buy event middleware - zod schema

--- app/backend/src/routes/events.ts
+++ app/backend/src/routes/events.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import EventsController from '../controllers/EventsController';
 import auth from '../middlewares/auth';
+import validateBuyEvent from '../middlewares/validateBuyEvent';
 import validateEvent from '../middlewares/validateEvent';
 
 const events = Router();
@@ -11,6 +12,6 @@ events.get('/', EventsController.getAll);
 events.get('/:id', EventsController.getById);
 events.put('/:id', EventsController.update);
 events.delete('/:id', EventsController.remove);
-events.post('/buy-ticket', EventsController.buyTicket);
+events.post('/buy-ticket', validateBuyEvent, EventsController.buyTicket);
 
 export default events;
