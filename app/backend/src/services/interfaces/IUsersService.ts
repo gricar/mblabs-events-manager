95ddc371a1cdddcb02ce51e6c2e4f66a36193fbe
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-12-06 20:38:49 -0300

    feat: get user by id - DTO - events

--- app/backend/src/services/interfaces/IUsersService.ts
+++ app/backend/src/services/interfaces/IUsersService.ts
@@ -1,11 +1,12 @@
 import { ILogin } from '../../entities/schemas/login';
 import { IUser } from '../../entities/schemas/user';
 import { User } from '../../entities/User';
+import { UserDTO } from '../../DTOs/UserDTO';
 
 export default interface IUsersService {
   create: (user: IUser) => Promise<Partial<User> | Error>;
   getAll: () => Promise<Partial<User>[]>;
-  getById: (id: string) => Promise<Partial<User> | Error>;
+  getById: (id: string) => Promise<Partial<UserDTO> | Error>;
   getByName: (username: string) => Promise<null | Error>;
   login: (userLogin: ILogin) => Promise<string>;
   update: (id: string, user: IUser) => Promise<boolean | Error>;
