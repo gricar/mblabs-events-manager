48be6cdce87aee98dc154631fce048463b2b9c9e
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-12-06 17:32:38 -0300

    refactor: event schema zod date schema validator amount of tickets included default value to sold out

--- app/backend/src/entities/schemas/event.ts
+++ app/backend/src/entities/schemas/event.ts
@@ -1,15 +1,22 @@
 import { z } from 'zod';
 
+const dateSchema = z.preprocess((arg) => {
+  if (typeof arg == 'string' || arg instanceof Date) return new Date(arg);
+}, z.date().min(new Date()));
+
 const EventSchema = z.object({
-  name: z.string(),
-  eventDay: z.date().min(new Date()),
+  name: z.string().min(4),
+  eventDay: dateSchema,
   peopleCapacity: z.number().positive(),
   ticketsAvailable: z.number().positive(),
-  soldOut: z.boolean(),
+  soldOut: z.boolean().default(false),
 });
 
 const CreateEventSchema = EventSchema.extend({
   sponsor: z.string(),
+}).refine((schema) => schema.ticketsAvailable <= schema.peopleCapacity, {
+  message: 'The amount of available tickets must be less than or equal to the capacity of people',
+  path: ['ticketsAvailable'],
 });
 
 type IEvent = z.infer<typeof EventSchema>;
